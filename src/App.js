54e854677e4575a14b90e2583d7d29a416030278
fcaumartin <fcaumartin@gmail.com>
2023-03-09 15:44:30 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [liste, setListe] = useState([]);
+
+  useEffect( () => {
+      axios
+        .get("https://127.0.0.1:8000/api/produits", {
+          headers: {
+            Accept: "application/json"
+          }
+        })
+        .then ( (reponse) => {
+          console.log(reponse)
+          setListe(reponse.data)
+        })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      liste des produits
+
+      {
+        liste.map( (pro, i) => (
+          <div key={i}>
+            {pro.libelle} - <b>{pro.prix}</b>
+          </div>
+        ))
+      }
     </div>
   );
 }
